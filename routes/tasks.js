d9898392b8f50cf2b2965a949345c4d81992e9e6
rezzworks <john.beasley.webdev@gmail.com>
2019-03-17 22:37:55 -0400

    update

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb+srv://userJohn:w1dg3ts@mydatabase-fboxm.mongodb.net/test?retryWrites=true', ['tasks']);
 
 router.get('/tasks', function(req, res, next){
-    res.send('TASKS API');
+    db.tasks.find(function(err, tasks)
+    {
+        if(err)
+        {
+            res.send(err);
+        }
+        res.json(tasks);
+    });
 });
 
 module.exports = router;
